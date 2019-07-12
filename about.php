05ce0ef57a9e6c897c9ad39a52d7f80d5bb7d110
usama abid <38359030+usamathegreat@users.noreply.github.com>
2019-07-12 16:00:12 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <meta name="keywords" content="1 inch submersible pump,110v water pump,
+12 volt submersible pump,
+120v water pump,
+12kw diesel generator,
+12v water pump">
+
+    <title>About</title>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet"  type="text/css" href="css1.css">
+        <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+  <div class="container-fluid row bg-white">
+   <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+       <div class="row">
+       <div class="col-md-3 text-center mt-4 p-3">
+        <div class="row align-items-center position-relative">
+            <div class="site-logo">
+                <a href="index.php" class="text-black"><span class="text-primary" style="font-size:22px">MODERN MACHINES</span></a>
+            </div>
+            </div>
+           </div>
+
+            <div class="col-md-9">
+           <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link " href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="forget.php">Forget-password</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="signup.php">SignUp</a>
+      </li>
+      
+      <li class="nav-item active">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contacts.php">Contact-Us</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+            </div>
+
+        </div>
+      </div>
+    </header>
+
+          </div>
+<h1 class="h1 ml-4 mt-4">ABOUT</h1>
+<p class="container p text-success" style="margin-top:50px;font-size:20px;">
+    Modren Machinery is undoubtedly synonymous with state of the art merchandising and unparalleled professional caliber. Moving way beyond the Korean shores, today Hyundai with a worldwide sales network in over 193 countries, has a standing of its very own in the USA, UK, Europe, UAE and South Asia.
+
+    In Pakistan too, the brand name has made a definite mark for its high quality and outstanding value. Combining its numerous worldwide offices and prompt and extensive information gathering capability with advanced trading techniques and financing ability, Hyundai Corporation provides optimum solution to its customer’s various requirements. Especially keeping in view the local perspective in Pakistan, Hyundai has introduced high-end power products, comprising of Gensets, Inverters, UPS systems, etc. The super quality and cost effectiveness associated with Hyundai products has won the company the confidence and trust of customers within an amazingly short period of time.
+
+    In the year 2012 the CMC Group was approached and awarded the contract to become the sole importer / manufacturer for Hyundai Power Equipment for Pakistan by Hyundai Corporation, Korea, based on our knowledge and machinery sales, as well as engineering, support and maintenance expertise.
+
+    Consequently, the CMC Group of Companies takes due pride in sharing the role of industry leadership with Hyundai in Pakistan. We are equally aware of shouldering the responsibility to deliver qualitatively and up to the mark of our customers’ utmost satisfaction.
+</p>
+ <footer>
+        <div class="text-center text-dark">
+            <p>
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved.
+            </p>
+            </div>
+    </footer>
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
